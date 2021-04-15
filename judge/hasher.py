113a4952bb521c9b76ec3b4a6418a30c4767afeb
Steven Guikal <void@fluix.dev>
2021-04-14 21:34:36 -0400

    Fix hasher

--- judge/hasher.py
+++ judge/hasher.py
@@ -2,6 +2,7 @@ from collections import OrderedDict
 
 from argon2 import PasswordHasher
 from django.contrib.auth.hashers import BasePasswordHasher, mask_hash
+from django.utils.translation import gettext_lazy as _
 
 
 class SpecialECOOPasswordHasher(BasePasswordHasher):
